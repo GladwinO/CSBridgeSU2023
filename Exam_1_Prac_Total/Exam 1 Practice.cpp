736a0088c220ae3376b5a4ddb44be490c2f953d9
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-08-09 15:25:04 -0400

    Add files via upload

---
+++ Exam_1_Prac_Total/Exam 1 Practice.cpp
@@ -0,0 +1,139 @@
+// Exam 1 Practice.cpp : 
+
+//Set Theory 
+
+/*
+1)
+
+a) TRUE
+b) FALSE
+c) TRUE
+d) FALSE
+e) FALSE
+f) FALSE
+g) TRUE
+h) FALSE
+i) FALSE
+j) TRUE
+
+1.2
+C IS FALSE 
+
+//////////////////////////
+
+
+FUNCTIONS (important note: if a function maps to a number not within the target the function is not well defined
+Z to Z+
+
+A) one-to-one but not onto 
+b) onto but not one-to-one
+c) neither one-to-one nor onto
+d) one-to-one but not onto
+e)one-to-one and onto
+
+/////////////////////////
+
+PROOFS
+
+4.1 Direct Proof
+
+    let x and y be 2 odd integers. x = 2k + 1 and y = 2k + 1 where k is an integer
+    the product of x and y (xy) = 2k + 1
+
+    (2k + 1) * (2k + 1) = 4k^2 + 4k + 1 =
+
+
+4.2 Proof by contrapositive
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////
+
+Number System Conversion 
+
+5.1 (-43) base 10=
+
+    (43) base 10
+        2^6 = 64
+        43 - 64 = -21 so this does not work and is the start (bit loc 6 = 0)
+        2^5 = 32
+        43-32 = 11 (bit loc 5 = 1)
+        2^4 = 16 
+        11 - 16 = -5 (bit loc 4 = 0)
+        2^3 = 8 
+        11 - 8 = 3 (bit loc 3 = 1)
+        2^2 = 4
+        3-4 = -1 (bit loc 2 = 0)
+        2^1 = 2
+        2 - 1 = 1 (bit loc 1 = 1)
+        2^) = 1
+        1 - 1 = 0 (bit loc 0 = 1)
+
+        (43) base 10 = 00101011
+
+        Now:
+          00101011
+        - ????????
+         100000000
+
+         so...
+          00101011
+        - 11010101
+         100000000
+
+         Answer = (11010101) 8-bit Two's Complement
+
+5.2 Binary to hexa
+    (110011100)base 2 = 
+        separate the binary code into groups of four starting from the right
+        (0001 1001 1100) 
+        find the corresponding number for each group in hexa.
+        1100 base 2 = 12 = C base 16
+        1001 base 2 = 9 = 9 base 16
+        0001 base 2 = 1 = 1 base 16
+
+        Answer = (19C) base 16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
