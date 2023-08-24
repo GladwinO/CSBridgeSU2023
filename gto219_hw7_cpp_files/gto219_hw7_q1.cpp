ba13b7c831b13601c117dcd5385ae6605f4565f6
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-08-24 13:51:34 -0400

    Add files via upload

--- gto219_hw7_cpp_files/gto219_hw7_q1.cpp
+++ gto219_hw7_cpp_files/gto219_hw7_q1.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 
 int printMonthCalender(int numOfDays, int startingDay);
+bool isLeapYear(int year);
+void printYearCalender(int year, int startingDay);
 
 int main()
 {
     int numOfDaysInt, startingDayInt;
-    int lastDayOfMonth;
+    int lastDayOfMonthInt, userYearInt;
     std::cout << "Please enter the number of days in the month: ";
     std::cin >> numOfDaysInt;
 
@@ -16,9 +18,31 @@ int main()
         << "Friday = 5\nSaturday = 6\nSunday = 7\nEnter your number: ";
     std::cin >> startingDayInt;
 
-    lastDayOfMonth = printMonthCalender(numOfDaysInt, startingDayInt);
-    std::cout << "\n" << lastDayOfMonth;
+    std::cout << "\n\nSection A:\n";
+    lastDayOfMonthInt = printMonthCalender(numOfDaysInt, startingDayInt);
+    std::cout << "\nLast day of the month(Mon-Sun: 1-7) is: " << lastDayOfMonthInt;
 
+    std::cout << "\n\nSection B:\n";
+    std::cout << "Please enter a year: ";
+    std::cin >> userYearInt;
+    if (isLeapYear(userYearInt)) {
+        std::cout << userYearInt << " is a leap year.";
+    }
+    else {
+        std::cout << userYearInt << " is not a leap year.";
+    }
+
+    std::cout << "\n\nSection C/D:\n";
+    std::cout << "Please enter a year: ";
+    std::cin >> userYearInt;
+    std::cout << "Please enter a number from 1-7 which represents " <<
+        "the starting day:\nMonday = 1\nTuesday = 2\nWednesday = 3\nThursday = 4\n"
+        << "Friday = 5\nSaturday = 6\nSunday = 7\nEnter your number: ";
+    std::cin >> startingDayInt;
+    std::cout << "\n";
+    printYearCalender(userYearInt, startingDayInt);
+
+    return 0;
 
 }
 
@@ -27,6 +51,8 @@ int printMonthCalender(int numOfDays, int startingDay) {
     int initNumTabsInt = 0;
     int daysInTheWeekInt = startingDay;
     int dayOfWeekInt = startingDay;
+    int lastDayOFLastWeek = (startingDay + numOfDays) - 1;
+    
     switch (startingDay) {
     case 1:
         initNumTabsInt = 0;
@@ -60,7 +86,7 @@ int printMonthCalender(int numOfDays, int startingDay) {
     }
     for (int i = 1; i <= numOfDays; i++) {
         std::cout << i << "\t";
-        if (daysInTheWeekInt % numDaysInWeek == 0) {
+        if (daysInTheWeekInt % numDaysInWeek == 0 && daysInTheWeekInt < lastDayOFLastWeek) {
             std::cout << "\n";
         }
         daysInTheWeekInt++;
@@ -74,14 +100,106 @@ int printMonthCalender(int numOfDays, int startingDay) {
             }
         }
 
-
-
-
-
-
     }
-
+    
 
 
     return dayOfWeekInt;
+}
+
+bool isLeapYear(int num) {
+    const int lowNum = 4;
+    const int midNum = 100;
+    const int highNum = 400;
+    int divLowRemInt, divMidRemInt, divHighRemInt;
+    divLowRemInt = num % lowNum;
+    divMidRemInt = num % midNum;
+    divHighRemInt = num % highNum;
+
+    if (divLowRemInt == 0 && divMidRemInt != 0) {
+        return true;
+    }
+    else if (divLowRemInt == 0 && divMidRemInt == 0) {
+        if (divHighRemInt == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    else {
+        return false;
+    }
+
+}
+
+void printYearCalender(int year, int startingDay) {
+    int month = 1;
+    int numDaysInMonth;
+    int lastDayPreviousMonth, newStartingDay = startingDay;
+    while (month < 13) {
+        switch (month) {
+        case 1:
+            std::cout << "January " << year <<std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 2:
+            std::cout << "February " << year << std::endl;
+            if (isLeapYear(year)) {
+                numDaysInMonth = 29;
+            }
+            else {
+                numDaysInMonth = 28;
+            }
+            break;
+        case 3:
+            std::cout << "March " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 4:
+            std::cout << "April " << year << std::endl;
+            numDaysInMonth = 30;
+            break;
+        case 5:
+            std::cout << "May " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 6:
+            std::cout << "June " << year << std::endl;
+            numDaysInMonth = 30;
+            break;
+        case 7:
+            std::cout << "July " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 8:
+            std::cout << "August " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 9:
+            std::cout << "September " << year << std::endl;
+            numDaysInMonth = 30;
+            break;
+        case 10:
+            std::cout << "October " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        case 11:
+            std::cout << "November " << year << std::endl;
+            numDaysInMonth = 30;
+            break;
+        case 12:
+            std::cout << "December " << year << std::endl;
+            numDaysInMonth = 31;
+            break;
+        }
+        lastDayPreviousMonth = printMonthCalender(numDaysInMonth, newStartingDay);
+        std::cout << "\n\n";
+        
+        newStartingDay = lastDayPreviousMonth + 1;
+        if (newStartingDay > 7) {
+            newStartingDay = 1;
+        }
+        month++;
+    }
 }
