bd572149c8c6973d1a7227afaca959557bbb0a16
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-09-07 13:13:43 -0400

    Update gto219_hw9_q3.cpp

--- gto219_hw9_cpp_files/gto219_hw9_q3.cpp
+++ gto219_hw9_cpp_files/gto219_hw9_q3.cpp
@@ -7,26 +7,29 @@ void printArr(int* arr, int arrSize);
 int* createArr(int arrSize);
 int* getPosNums1(int* arr, int arrSize, int& outPosArrSize);
 int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr);
-void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize)
+void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize);
+void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr);
 
 
 int main()
 {
-    int arrSize, arrSizePos1, posNum2Size, posNum3Size;
+    int arrSize, arrSizePos1, posNum2Size, posNum3Size, posNum4Size;
     std::cout << "Please provide the number of integers that will be entered: ";
     std::cin >> arrSize;
     int* userArr;
-    int* userPosArr1, *userPosArr2, *userPosArr3;
+    int* userPosArr1 = nullptr, * userPosArr2 = nullptr, * userPosArr3 = nullptr, * userPosArr4 = nullptr;
     int* outPosArrSizePtr = &posNum2Size;
+    int* outPosArrSizePtr2 = &posNum4Size;
 
     std::cout << "Please enter " << arrSize << " numbers separated by a space or enter:\n";
     userArr = createArr(arrSize);
+    std::cout << std::endl;
 
     std::cout << "Section A:\n";
     userPosArr1 = getPosNums1(userArr, arrSize, arrSizePos1);
     std::cout << "The number of positve integers in the array is " << arrSizePos1 << std::endl;
     printArr(userPosArr1, arrSizePos1);
-    
+
 
     std::cout << "\n\nSection B:\n";
     userPosArr2 = getPosNums2(userArr, arrSize, outPosArrSizePtr);
@@ -35,13 +38,21 @@ int main()
 
 
     std::cout << "\n\nSection C:\n";
-    //last place i stopped working
-    
+    getPosNums3(userArr, arrSize, userPosArr3, posNum3Size);
+    std::cout << "The number of positve integers in the array is " << posNum3Size << std::endl;
+    printArr(userPosArr3, posNum3Size);
+
+    std::cout << "\n\nSection D:\n";
+    getPosNums4(userArr, arrSize, &userPosArr4, outPosArrSizePtr2);
+    std::cout << "The number of positve integers in the array is " << *outPosArrSizePtr2 << std::endl;
+    printArr(userPosArr4, *outPosArrSizePtr2);
+
 
 
     delete[] userArr;
     delete[] userPosArr1;
     delete[] userPosArr2;
+    delete[] userPosArr3;
 
     userArr = nullptr;
     userPosArr1 = nullptr;
@@ -88,7 +99,7 @@ int* getPosNums1(int* arr, int arrSize, int& outPosArrSize) {
 }
 
 int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
-    
+
     *outPosArrSizePtr = numOfPosInts(arr, arrSize);
     int* posArr = new int[*outPosArrSizePtr];
     int posArrLoc = 0;
@@ -100,4 +111,30 @@ int* getPosNums2(int* arr, int arrSize, int* outPosArrSizePtr) {
     }
 
     return posArr;
+}
+
+void getPosNums3(int* arr, int arrSize, int*& outPosArr, int& outPosArrSize) {
+   
+    outPosArrSize = numOfPosInts(arr, arrSize);
+    outPosArr = new int[outPosArrSize];
+    int posArrLoc = 0;
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] > 0) {
+            outPosArr[posArrLoc] = arr[i];
+            posArrLoc++;
+        }
+    }
+}
+
+void getPosNums4(int* arr, int arrSize, int** outPosArrPtr, int* outPosArrSizePtr) {
+    
+    *outPosArrSizePtr = numOfPosInts(arr, arrSize);
+    *outPosArrPtr = new int[*outPosArrSizePtr];
+    int posArrLoc = 0;
+    for (int i = 0; i < arrSize; i++) {
+        if (arr[i] > 0) {
+            (*outPosArrPtr)[posArrLoc] = arr[i];
+            posArrLoc++;
+        }
+    }
 }
