b0a5f2ee583e6089ffb085bc09e6fd8b0c7b1825
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-10-11 16:05:34 -0400

    Add files via upload

--- gto219_hw13.cpp
+++ gto219_hw13.cpp
@@ -1,6 +1,8 @@
 // gto219_hw13.cpp:
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 
 //area for const variables
 const int numRows = 4;
@@ -31,7 +33,9 @@ public:
     void setYPos(int yPosition);
     void setMoves(int userNumMoves);
     void setSymb(char userSymb); //i only want the derived classes using it so should it be protected instead?
+    //virtual void Move(Organism* orgArrPtr);//still trying to figure out what the parameters should be
     friend std::ostream& operator<<(std::ostream& outs, const Organism& bug);
+
 };
 
 class Ant : public Organism {
@@ -54,10 +58,28 @@ public:
     friend std::ostream& operator <<(std::ostream& outs, const Doodlebug& bug);
 };
 
+/// non-member functions declaration list
+void createBaseGrid(Organism* orgArrPtr[][numCols]);
+
+void createPlayableGrid(Organism* orgArrPtr[][numCols]);
+
+void printGrid(Organism* orgArrPtr[][numCols]);
+
+void deleteGrid(Organism* orgArrPtr[][numCols]);
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Organism* orgArrPtr[numRows][numCols];
+    createBaseGrid(orgArrPtr);
+    printGrid(orgArrPtr);
+    createPlayableGrid(orgArrPtr);
+
+    
+    printGrid(orgArrPtr);
+    deleteGrid(orgArrPtr);
+
+    
 }
 
 //Class defintions
@@ -152,4 +174,65 @@ Doodlebug::Doodlebug(int userXPos, int userYPos, int moves) : Organism(userXPos,
 std::ostream& operator <<(std::ostream& outs, const Doodlebug& bug) {
     outs << bug.getSymb();
     return outs;
+}
+
+
+/// Non-member fucntion Definition list
+
+void createBaseGrid(Organism* orgArrPtr[][numCols]) {
+    //creates a 2D array of organism objects
+    //creates a copy of the pointer, but the pointer still points to the same memory addresses
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            orgArrPtr[i][j] = new Organism(i, j);
+        }
+    }
+}
+
+void createPlayableGrid(Organism* orgArrPtr[][numCols]) {
+    //sets the Ants and Doodlebugs on the Grid
+    //creates a copy of the pointer, but the pointer still points to the same memeory addresses
+    std::srand(std::time(nullptr));
+    int bugCount = 0;
+    while (bugCount < maxBugs) {
+        int xLoc = std::rand() % 4;
+        int yLoc = std::rand() % 4;
+        if (orgArrPtr[xLoc][yLoc]->getSymb() == '-') {
+            delete orgArrPtr[xLoc][yLoc];
+            orgArrPtr[xLoc][yLoc] = nullptr;
+            if (bugCount < maxAnts) {
+                orgArrPtr[xLoc][yLoc] = new Ant(xLoc, yLoc);
+            }
+            else {
+                orgArrPtr[xLoc][yLoc] = new Doodlebug(xLoc, yLoc);
+            }
+            bugCount++;
+        }
+        else {
+            continue;
+        }
+    }
+}
+
+void printGrid(Organism* orgArrPtr[][numCols]) {
+    // current print array function
+    //creates a copy of the pointer, but the pointer still points to the same memory addresses
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            std::cout << *orgArrPtr[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+    std::cout << std::endl;
+}
+
+void deleteGrid(Organism* orgArrPtr[][numCols]) {
+    //deletes memory element wise, sets elements to nullptr, then deletes the array
+    //creates a copy of the pointer, but the pointer still points to the same memory addresses
+    for (int i = 0; i < numRows; i++) {
+        for (int j = 0; j < numCols; j++) {
+            delete orgArrPtr[i][j];
+            orgArrPtr[i][j] = nullptr;
+        }
+    }
 }
