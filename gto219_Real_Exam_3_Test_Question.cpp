a3d0699753859fe2fe5c74ecf7ecf8854745bdfe
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-11-02 17:06:36 -0400

    Add files via upload

--- gto219_Real_Exam_3_Test_Question.cpp
+++ gto219_Real_Exam_3_Test_Question.cpp
@@ -7,11 +7,22 @@
 
 void inToPostFixConv(const std::string& inFix, std::queue<char>& queueBuffer);
 
+int postFixToValConv(std::queue<char> postFix);
+int postFixToVal(std::queue<char> postFix);
+
 int main()
 {
     std::string inFix = "a+b*c+(d*e+f)*g";
     std::queue<char> postFix;
-    inToPostFixConv(inFix, postFix);
+    int val;
+    //inToPostFixConv(inFix, postFix);
+    std::string postFixStr = "6523+8*+3+*";
+    for (char c : postFixStr) {
+        postFix.push(c);
+    }
+    val = postFixToVal(postFix);
+    //val = postFixToValConv(postFix);
+    std::cout << val;
     /*while (!postFix.empty()) {//this test inFix to postFix conversion
         std::cout << postFix.front();
         postFix.pop();
@@ -96,4 +107,105 @@ void inToPostFixConv(const std::string& inFix, std::queue<char>& queueBuffer) {
         queueBuffer.push(top);
         stackBuffer.pop();
     }
+}
+
+
+int postFixToValConv(std::queue<char> postFix) {
+    std::stack<int> stackBuffer;
+    char front = ' ';
+    if (!postFix.empty()) {
+        front = postFix.front();
+    }
+    while (!postFix.empty()) {
+        while (front != '*' && front != '/' && front != '+' && front != '-') {
+            int num = front - '0';
+            stackBuffer.push(num);
+            postFix.pop();
+            if (!postFix.empty()) {
+                front = postFix.front();
+            }
+            else {
+                break;
+            }
+        }
+        while ((front == '*' || front == '/' || front == '+' || front == '-') && !stackBuffer.empty() && !postFix.empty()) {
+            int operandLeft = 0, operandRight = 0;
+            operandRight = stackBuffer.top();
+            stackBuffer.pop();
+            if (!stackBuffer.empty()) {
+                operandLeft = stackBuffer.top();
+                stackBuffer.pop();
+            }
+            if (front == '*') {
+                postFix.pop();
+                stackBuffer.push(operandLeft * operandRight);
+                if (!postFix.empty()) {
+                    front = postFix.front();
+                }
+            }
+            else if (front == '/') {
+                postFix.pop();
+                stackBuffer.push(operandLeft / operandRight);
+                if (!postFix.empty()) {
+                    front = postFix.front();
+                }
+            }
+            else if (front == '+') {
+                postFix.pop();
+                stackBuffer.push(operandLeft + operandRight);
+                if (!postFix.empty()) {
+                    front = postFix.front();
+                }
+            }
+            else if (front == '-') {
+                postFix.pop();
+                stackBuffer.push(operandLeft - operandRight);
+                if (!postFix.empty()) {
+                    front = postFix.front();
+                }
+            }
+        }
+    }
+    if (stackBuffer.size() == 1) {
+        return stackBuffer.top();
+    }
+    return 0;
+}
+
+int postFixToVal(std::queue<char> postFix) {//this one is faster to right so better for the test
+    std::stack<int> stackBuffer;
+    char front = ' ';
+    while (!postFix.empty()) {
+        front = postFix.front();
+        postFix.pop();
+        if (front != '*' && front != '/' && front != '+' && front != '-') {
+            int num = front - '0';
+            stackBuffer.push(num);
+        }
+        else {
+            if (stackBuffer.size() < 2) {
+                return 0;
+            }
+            int operandRight = stackBuffer.top();
+            stackBuffer.pop();
+            int operandLeft = stackBuffer.top();
+            stackBuffer.pop();
+            if (front == '*') {
+                stackBuffer.push(operandLeft * operandRight);
+            }
+            else if (front == '/') {
+                stackBuffer.push(operandLeft / operandRight);
+            }
+            else if (front == '+') {
+                stackBuffer.push(operandLeft + operandRight);
+            }
+            else if (front == '-') {
+                stackBuffer.push(operandLeft - operandRight);
+            }
+        }
+    }
+    if (stackBuffer.size() == 1) {
+        return stackBuffer.top();
+    }
+    return 0;
 }
