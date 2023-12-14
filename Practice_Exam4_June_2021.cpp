82c3cd1764e05c52532b3e5337cef8ca007f03fc
Gladwin <59264329+GladwinO@users.noreply.github.com>
2023-12-14 14:22:19 -0500

    Add files via upload

--- Practice_Exam4_June_2021.cpp
+++ Practice_Exam4_June_2021.cpp
@@ -1,20 +1,28 @@
-// Practice_Exam4_June_2021.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// Practice_Exam4_June_2021.cpp:
 
 #include <iostream>
+#include <vector>
+
+void createTreeVec(std::vector<int>& newVec, BSTNode<int>* root) {
+    if (root == nullptr) {
+        return;
+    }
+    createTreeVec(newVec, root->left);
+    newVec.push_back(root->val);
+    createTreeVec(newVec, root->right);
+}
+
+bool treeMatchVec(const std::vector<int>& userVec, BSTNode<int>* root) {
+    std::vector<int> treeVec;
+    createTreeVec(treeVec, root);
+    if (treeVec == userVec) {
+        return true;
+    }
+    return false;
+}
+
 
 int main()
 {
     std::cout << "Hello World!\n";
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
